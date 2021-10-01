904910ddf0bc3d46fdf0f25a85f4a6b4c316649f
S3len3Ch4vz <nasaya.369@gmail.com>
2021-09-30 21:40:43 -0500

    Se crea la carpeta Js con su archivo y el archivo index.html

---
+++ js/script.js
@@ -0,0 +1,77 @@
+//Datos Primitivos
+
+34
+'este es una cadena'
+"este es una cadena"
+true
+undefined
+null
+
+//Objeto 
+
+{nombre :"Selene"}
+
+//Array
+
+[1,2,3]
+
+//Estructura de objetos 
+
+let perro ={
+    nombre="huesos",
+    raza='golden thetriver',
+};
+
+console.log(perro,nombre);
+console.log(perro['nombre'])
+
+//Estructura de Array 
+let perros = [
+  {
+    nombre: 'huesos',
+    raza:'galgo ingles',
+  },
+  {
+    nombre: 'solobino',
+    raza:'galgo ingles',
+    apodos:[apodo2,3],
+
+    },
+];
+
+console.log(perros[1]);
+console.log(perros)
+
+//declarar
+//let nombre;
+//const nombre;
+var nombre ='Selene';
+
+let nombreConLet = 'Caroline';
+
+const perro = {
+    nombre: "Huesos",
+    raza:'galgo'
+}
+
+perro.nombre ='otro nombre';
+console.log(perro);
+
+//Inicializar 
+let numero=1;
+
+
+//Funciones 
+
+//Expresión
+
+let miFunction = function (){
+
+}
+
+//Declarativa 
+
+function suma () {
+
+}
+
