e8afd7c06042950cfbac33b77fb513f146d3ddb4
S3len3Ch4vz <nasaya.369@gmail.com>
2021-10-08 19:34:36 -0500

    Inicio de la sesion 6

---
+++ Sesion_6/Js/script.js
@@ -0,0 +1,96 @@
+/* ------------------------------------ EJERCICIOS DE PRACTICA----------------------------------- */
+/*
+Tomando en cuenta el array songsData 
+
+Ejercicio 1
+input: 'The Cure'
+output: {
+  {
+    name: "Just like Heaven",
+    band: "The Cure",
+    releaseYear: "1980",
+    statistics: {
+      likes: 2545109856,
+      reproductions: 4324309,
+    }
+  }
+}
+----------------------------
+Ejercicio 2
+-Generar el top 5 de las canciones mas escuchadas
+-Mostrar la cancion mas reciente
+-Mostrar el artista mas gustado a los fans
+
+
+let songsData = [
+  {
+    name: "Just like Heaven",
+    band: "The Cure",
+    releaseYear: "1980",
+    statistics: {
+      likes: 2545109856,
+      reproductions: 4324309,
+    }
+  },
+  {
+    name: "Nikita",
+    band: "Elton John",
+    releaseYear: "1980",
+    statistics: {
+      likes: 9409483256,
+      reproductions: 9843293,
+    }
+  },
+  {
+    name: "Kashmir",
+    band: "Led Zeppelin",
+    releaseYear: "1980",
+    statistics: {
+      likes: 20000,
+      reproductions: 8000,
+    }
+  },
+  {
+    name: "Smells Like Teen Spirit",
+    band: "Nirvana",
+    releaseYear: "1989",
+    statistics: {
+      likes: 25000,
+      reproductions: 100000,
+    }
+  }, {
+    name: "So What",
+    band: "Metallica",
+    releaseYear: "1990",
+    statistics: {
+      likes: 12000,
+      reproductions: 95000,
+    }
+  }, {
+    name: "Nothing Else Matters",
+    band: "Metallica",
+    releaseYear: "1992",
+    statistics: {
+      likes: 128000,
+      reproductions: 915000,
+    }
+  }, {
+    name: "Daze",
+    band: "Poets of the Fall",
+    releaseYear: "2014",
+    statistics: {
+      likes: 3548413,
+      reproductions: 87095138,
+    }
+  }, {
+    name: "The Sweet Scape",
+    band: "Poets of the fall",
+    releaseYear: "2018",
+    statistics: {
+      likes: 26268856,
+      reproductions: 2424568,
+    }
+  }
+]
+*/
+
