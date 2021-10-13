d6f862493489b26b99bf3991cca7ad2c8a5b0f89
S3len3Ch4vz <nasaya.369@gmail.com>
2021-10-12 22:13:31 -0500

    Inicio sesion8

--- Sesion_7/Js/script.js
+++ Sesion_7/Js/script.js
@@ -119,4 +119,99 @@ const mentorsArray = [
   }
   ]
 
+/* el promedio de las materias se saca con la suma del valor de 
+todas las materias entre el numero total de materias. */
+
+// -Obtener el score promedio de cada materia( HTML, CSS, JS, ReactJS )
+
+const promedioMateria = (mentorsArray, nombreMateria) => {
+    let numeroDeMaterias = mentorsArray.length
+    let suma = 0
+    let promedio = 0
+    // const materiasArray = []
+  
+    mentorsArray.forEach((profesor) => {
+      //Dentro de objeto profesor
+      profesor.scores.forEach((materia) => {
+        //Dentro de objeto materias
+        if (materia.signature === nombreMateria) {
+          suma += materia.score
+        }
+      })
+      promedio = suma / numeroDeMaterias
+    })
+  
+    // materiasArray.push({ materia: nombreMateria, promedio })
+    // return promedio
+    // return materiasArray
+    return { materia: nombreMateria, promedio }
+  }
+  
+  //Obtener la lista de mentores cuyo promedio sea mayor a 9.5
+  
+  const obtenerLista = (mentorsArray) => {
+    let suma = 0
+    let cantidadDeMaterias = 4
+    let promedio = 0
+  
+    const mayora = []
+  
+    const mentors = mentorsArray.forEach((profesor) => {
+      //Dentro de objeto profesor
+      suma = 0
+      profesor.scores.forEach((materia) => {
+        //Dentro de objeto materias
+        cantidadDeMaterias = profesor.scores.length
+  
+        promedio = (suma += materia.score) / cantidadDeMaterias
+  
+        if (promedio > 9.5) {
+          mayora.push({ name: profesor.name, promedio })
+        }
+      })
+    })
+  
+    return mayora
+  }
+  
+  // Obtener el promedio individual de cada mentor
+  
+  const obtenerPromedioIndividual = (mentorsArray) => {
+    const promedios = []
+    mentorsArray.forEach((profesor) => {
+      //Dentro de objeto profesor
+      let suma = 0
+      let promedio = 0
+      profesor.scores.forEach((materia) => {
+        //Dentro de objeto materias
+        suma += materia.score
+      })
+      promedio = suma / profesor.scores.length
+      promedios.push({ name: profesor.name, promedio })
+    })
+  
+    return promedios
+  }
+  
+  /* -crear un array de strings con la siguiente forma:
+      "Mi nombre es {nombre} y mi promedio es de {promedio} 
+  */
+  
+  const createStringsArray = (mentorsArray) => {
+    const profes = []
+  
+    mentorsArray.forEach((profesor) => {
+      //Dentro de objeto profesor
+      let suma = 0
+      let promedio = 0
+  
+      profesor.scores.forEach((materia) => {
+        //Dentro de objeto materias
+        suma += materia.score
+      })
+      promedio = suma / profesor.scores.length
+      profes.push(`Mi nombre es ${profesor.name} y mi promedio es de ${promedio}`)
+    })
+    return profes
+  }
 
